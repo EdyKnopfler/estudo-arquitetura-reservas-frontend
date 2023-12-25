6a117c79632d57e32a3378b27f492a9ea94e3ca4
Éderson Cássio <edersoncassio1983@gmail.com>
2023-12-25 11:45:31 -0300

    Layout para área protegida

--- src/app/checkout/flight-going/page.tsx
+++ src/app/checkout/flight-going/page.tsx
@@ -1,6 +1,6 @@
-import { getCurrentFlightGoing } from "../fake-data/current-selections"
-import { getFlightsFromTo } from "../fake-data/flights"
-import Scheduler from "../ui/scheduler/scheduler"
+import { getCurrentFlightGoing } from "../../fake-data/current-selections"
+import { getFlightsFromTo } from "../../fake-data/flights"
+import Scheduler from "../../ui/scheduler/scheduler"
 
 export default async function Page() {
   const userId = 1  // TODO autenticação
