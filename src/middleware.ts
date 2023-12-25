9a5d45a195a4d967463bbaab173889058a44713a
Éderson Cássio <edersoncassio1983@gmail.com>
2023-12-25 11:16:30 -0300

    Removendo toda a tranqueira e tentando recomeçar

--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { withAuth } from "next-auth/middleware";
-import { authConfig } from "./lib/auth.config";
-
-export default withAuth(authConfig)
-
-export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-};
